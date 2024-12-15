d9d0d91b8583071782f1520dbe051018d97bfb6f
matheusscode <matheussdev3@gmail.com>
2024-12-14 22:20:57 -0400

    chore: config build files and change little configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,18 @@
 import pluginJs from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["packages/**/*.{js,mjs,cjs,ts}"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  },
+  { files: ["**/*.{ts}"] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 ];
