df5e46becf0233adfa0934d13081d9599437bc00
matheusscode <matheussdev3@gmail.com>
2024-12-16 09:43:50 -0400

    fix(page): resolve type error for async params in Props

--- docs/app/docs/[slug]/page.tsx
+++ docs/app/docs/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import { getMDX } from "@/utils/mdx-handlers";
 
 interface Props {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 }
 
 export default async function Page({ params }: Props) {
