cf6cfd417fa28c81ac2e40ca384c7624f1c331ca
matheussatoshi1 <matheussdev3@gmail.com>
2025-01-17 19:04:34 -0400

    feat: adds client companies

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,13 +1,17 @@
 import Copy from "@/components/markdown/copy";
 import { buttonVariants } from "@/components/ui/button";
 import { page_routes } from "@/lib/routes-config";
+import { registry_enterprises } from "@/registry/registry-enterprises";
 import { TerminalSquareIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex sm:min-h-[85.5vh] min-h-[85vh] flex-col items-center justify-center text-center px-2  sm:pb-24">
-      <h1 className="text-4xl font-bold mb-4 sm:text-6xl">Native Variants</h1>
+      <h1 className="text-4xl font-bold sm:mt-56 mb-4 sm:text-6xl">
+        Native Variants
+      </h1>
       <p className="mb-8 sm:text-lg max-w-[800px] text-muted-foreground">
         Simplifying and Streamlining Style Variations with StyleSheet in React
         Native.
@@ -28,6 +32,21 @@ export default function Home() {
           content="npm install native-variants"
         />
       </span>
+
+      <div className=" sm:mt-24 flex flex-col justify-center items-center">
+        <h1 className="font-medium mb-1.5">Built for modern product teams.</h1>
+        <span className="text-muted-foreground">
+          From next-gen startups to established enterprises.
+        </span>
+
+        <div className="flex gap-10 mt-6">
+          {registry_enterprises.map((item) => (
+            <Link key={item.id} href={item.href} title={item.alt}>
+              <Image src={item.logo} alt={item.alt} width={40} height={40} />
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ docs/registry/registry-enterprises.ts
@@ -0,0 +1,24 @@
+import atemLogo from "@/public/atem.svg";
+import poshCashLogo from "@/public/poshcash.svg";
+import reamLogo from "@/public/ream.svg";
+
+export const registry_enterprises = [
+  {
+    id: 1,
+    logo: poshCashLogo,
+    alt: "Poshcash Enterprise",
+    href: "https://poshcash.com.br/",
+  },
+  {
+    id: 2,
+    logo: atemLogo,
+    alt: "Atem Enterprise",
+    href: "https://atem.com.br/",
+  },
+  {
+    id: 3,
+    logo: reamLogo,
+    alt: "Ream Enterprise",
+    href: "https://ream.com.br/",
+  },
+];
