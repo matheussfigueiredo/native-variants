e14e3d022a87fb2d1cb3e4c7735ef4f157a0bc8b
matheussatoshi1 <matheussdev3@gmail.com>
2025-06-04 23:41:40 -0400

    feat: add create-variants-provider for managing variant context and update dependencies

---
+++ src/create-provider.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type GenericVariantsContext<T> = {
+  variants: T;
+};
+
+export const createVariantsProvider = <T,>() => {
+  const Context = React.createContext<GenericVariantsContext<T> | undefined>(
+    undefined,
+  );
+
+  const Provider = ({
+    children,
+    variants,
+  }: {
+    variants: T;
+    children: React.ReactNode;
+  }) => {
+    return <Context.Provider value={{ variants }}>{children}</Context.Provider>;
+  };
+
+  const useVariants = () => {
+    const context = React.useContext(Context);
+
+    if (!context) {
+      throw new Error("useVariants must be used within its Provider");
+    }
+
+    return context.variants;
+  };
+
+  return {
+    Provider,
+    useVariants,
+  };
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./merge";
 export * from "./nv";
 export * from "./props";
+export * from './create-provider'
