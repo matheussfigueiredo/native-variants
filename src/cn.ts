eac5f72acb6949d9aad48b4f3e9bb8fe5f367600
matheussatoshi1 <matheussdev3@gmail.com>
2025-06-05 00:40:30 -0400

    chore: bump version to 0.1.41 and update cn function to use Partial<Styles>

--- src/cn.ts
+++ src/cn.ts
@@ -1,22 +1,6 @@
-import {
-  FlexStyle,
-  ImageStyle,
-  StyleProp,
-  TextStyle,
-  TransformsStyle,
-  ViewStyle,
-} from "react-native";
 import { Styles } from "./props";
 
-type CnStyleProp = Partial<
-  | StyleProp<ViewStyle>
-  | StyleProp<TextStyle>
-  | StyleProp<ImageStyle>
-  | StyleProp<FlexStyle>
-  | StyleProp<TransformsStyle>
->;
-
-export function cn(...styles: CnStyleProp[]): CnStyleProp {
+export function cn(...styles: Partial<Styles>[]): any {
   return styles
     .filter((style): style is Styles => Boolean(style))
     .reduce<Styles>(
