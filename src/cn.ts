71e4a34bfb47e8e0c24dde4e450131ed27d7eb71
matheussatoshi1 <matheussdev3@gmail.com>
2025-06-05 00:44:03 -0400

    fix: update cn function to use StyleProp<any>[] and return type to Styles

--- src/cn.ts
+++ src/cn.ts
@@ -1,6 +1,7 @@
+import { StyleProp } from "react-native";
 import { Styles } from "./props";
 
-export function cn(...styles: Partial<Styles>[]): any {
+export function cn(...styles: StyleProp<any>[]): Styles {
   return styles
     .filter((style): style is Styles => Boolean(style))
     .reduce<Styles>(
