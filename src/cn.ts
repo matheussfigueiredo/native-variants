1b9be121cb1ff8250484c649a22186f5aa1df2e0
matheussatoshi1 <matheussdev3@gmail.com>
2025-06-05 00:35:16 -0400

    chore: bump version to 0.1.38 and update react-native dependency to 0.79.3 refactor: enhance cn function to support additional style types

--- src/cn.ts
+++ src/cn.ts
@@ -1,7 +1,29 @@
-import { StyleProp } from "react-native";
+import {
+  FlexStyle,
+  ImageStyle,
+  StyleProp,
+  TextStyle,
+  TransformsStyle,
+  ViewStyle,
+} from "react-native";
+import { Styles } from "./props";
 
-export function cn<T = any>(
-  ...styles: (StyleProp<T> | false | null | undefined)[]
-): StyleProp<T>[] {
-  return styles.filter(Boolean) as StyleProp<T>[];
+type CnStyleProp = Partial<
+  | StyleProp<ViewStyle>
+  | StyleProp<TextStyle>
+  | StyleProp<ImageStyle>
+  | StyleProp<FlexStyle>
+  | StyleProp<TransformsStyle>
+>;
+
+export function cn(...styles: CnStyleProp[]): Styles {
+  return styles
+    .filter((style): style is Styles => Boolean(style))
+    .reduce<Styles>(
+      (merged, style) => ({
+        ...merged,
+        ...style,
+      }),
+      {},
+    );
 }
