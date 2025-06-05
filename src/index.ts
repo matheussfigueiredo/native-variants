fd331b1bb0ac771a17a1b4e86a7f3932a98f970a
matheussatoshi1 <matheussdev3@gmail.com>
2025-06-05 00:09:35 -0400

    refactor: update version to 0.1.33, add cn function, and remove merge function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from "./merge";
+export * from "./cn";
 export * from "./nv";
 export * from "./props";
 export * from './create-provider'
--- src/merge.ts
+++
@@ -1,7 +0,0 @@
-import { StyleProp } from "react-native";
-
-export function merge<T extends object>(
-  ...styles: Array<StyleProp<T> | null | undefined | false>
-): StyleProp<T> {
-  return styles.filter(Boolean) as StyleProp<T>;
-}
